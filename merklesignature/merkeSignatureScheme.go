0ccdd01b03284a4ad3fb2c43e8edbde8d22f9bac
Almog Tal <almog.tal@algorand.com>
2022-08-31 10:32:27 +0300

    CR: got rid of small file

--- merklesignature/merkeSignatureScheme.go
+++ merklesignature/merkeSignatureScheme.go
@@ -44,6 +44,13 @@ type (
 	}
 )
 
+// firstRoundInKeyLifetime calculates the round of the valid key for a given round by lowering to the closest KeyLiftime divisor.
+// It is implicitly assumed that round is larger than keyLifetime, as an MSS key for round 0 is not valid.
+// A key lifetime of 0 is invalid.
+func firstRoundInKeyLifetime(round, keyLifetime uint64) uint64 {
+	return round - (round % keyLifetime)
+}
+
 // MsgIsZero returns whether this is a zero value
 func (s *Signature) MsgIsZero() bool {
 	return ((*s).Signature.MsgIsZero()) && ((*s).VectorCommitmentIndex == 0) && ((*s).Proof.MsgIsZero()) && ((*s).VerifyingKey.MsgIsZero())
